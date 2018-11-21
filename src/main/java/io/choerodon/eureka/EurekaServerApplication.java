ce4eb65adacdeff0ec0acc48f37dac00fdc7dbf3
Fan <fan@choerodon.io>
2018-11-21 10:00:12 +0800

    [REM] delete kafka

--- src/main/java/io/choerodon/eureka/EurekaServerApplication.java
+++ src/main/java/io/choerodon/eureka/EurekaServerApplication.java
@@ -1,4 +1,6 @@
-package io.choerodon.framework.eureka;
+package io.choerodon.eureka;
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -10,8 +12,6 @@ import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * @author xausky
